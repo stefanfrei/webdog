e073ef39b84f126d78797ced362da16a5fd532ec
Stefan Frei <stefan.a.frei@gmail.com>
2021-04-05 22:54:52 +0200

    some ui-experience

--- src/main/java/org/schlibbuz/webdog/tools/PropsLoader.java
+++ src/main/java/org/schlibbuz/webdog/tools/PropsLoader.java
@@ -1,10 +1,20 @@
+// Copyright (c) 2021 Stefan Frei <stefan.a.frei@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 package org.schlibbuz.webdog.tools;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class PropsLoader {
+
+    private static final Logger w = LogManager.getLogger(PropsLoader.class);
 
     private PropsLoader() {}
     
@@ -14,8 +24,21 @@ public class PropsLoader {
             props.load(input);
             return props;
 
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } catch (IOException e) {
+            w.error(e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static String getVersion() {
+        try (InputStream input = PropsLoader.class.getClassLoader().getResourceAsStream("app.props")) {
+            Properties props = new Properties();
+            props.load(input);
+            return props.getProperty("version");
+
+        } catch (IOException e) {
+            w.error(e.getMessage());
         }
 
         return null;
--- src/main/java/org/schlibbuz/webdog/tools/UIExperience.java
+++ src/main/java/org/schlibbuz/webdog/tools/UIExperience.java
@@ -1,3 +1,8 @@
+// Copyright (c) 2021 Stefan Frei <stefan.a.frei@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 package org.schlibbuz.webdog.tools;
 
 import org.apache.logging.log4j.Logger;
@@ -8,7 +13,9 @@ public class UIExperience {
     private static final Logger w = LogManager.getLogger(UIExperience.class);
 
     public void sayWelcome() {
-        w.info("Welcome to webdog %s", UIExperience.class.getPackage().getImplementationVersion());
+        String version = UIExperience.class.getPackage().getImplementationVersion();
+        if (version == null) version = PropsLoader.getVersion();
+        w.info("Welcome to webdog {}", version);
     }
 
     public void reportWeather() {
