b1773a2b710532e31936fbe918666e04ebd85fce
Stefan Frei <stefan.a.frei@gmail.com>
2021-04-06 18:40:52 +0200

    add enum for webdriver types available in selenium

---
+++ src/main/java/org/schlibbuz/webdog/selenium/WebDriverType.java
@@ -0,0 +1,37 @@
+package org.schlibbuz.webdog.selenium;
+
+//     ChromeDriver, ChromiumDriver, EdgeDriver, EventFiringWebDriver, FirefoxDriver, InternetExplorerDriver, OperaDriver, RemoteWebDriver, SafariDriver
+public enum WebDriverType {
+    // types
+    CHROME("chrome"),
+    CHROMIUM("chromium"),
+    EDGE("edge"),
+    EVENT_FIRING_WEB("eventfiringweb"),
+    FIREFOX("firefox"),
+    INTERNET_EXPLORER("internetexplorer"),
+    OPERA("opera"),
+    REMOTE_WEB("remoteweb"),
+    SAFARI("safari");
+    // end
+
+
+    private final String name;
+
+
+    WebDriverType(String name) {
+        this.name = name;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+
+    public static WebDriverType fromName(String name) {
+        for(WebDriverType t : values()) {
+            if(t.name.equals(name)) return t;
+        }
+        return null;
+    }
+}
