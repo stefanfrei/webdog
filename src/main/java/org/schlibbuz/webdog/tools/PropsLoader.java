2fc6f444cf69712e222818d52839fc1af9c4f3bd
Stefan Frei <stefan.a.frei@gmail.com>
2021-04-06 12:37:17 +0200

    make method sync, so no lock is needed

--- src/main/java/org/schlibbuz/webdog/tools/PropsLoader.java
+++ src/main/java/org/schlibbuz/webdog/tools/PropsLoader.java
@@ -15,7 +15,6 @@ import org.apache.logging.log4j.LogManager;
 public class PropsLoader {
 
     private static final Logger w = LogManager.getLogger(PropsLoader.class);
-    private static final Object lock = new Object();
     private static final String PROPS_FILE_DEFAULT = "app.props";
     private static final String PROPS_PREFIX = "webdog.";
 
@@ -27,22 +26,19 @@ public class PropsLoader {
         PropsLoader.addSysProps(PROPS_FILE_DEFAULT);
     }
 
-    public static void addSysProps(final String propsFile) {
-        synchronized(lock) {
-            try (InputStream input = PropsLoader.class.getClassLoader().getResourceAsStream(propsFile)) {
-                var props = new Properties();
-                props.load(input);
-                props.forEach( (k, v) -> {
-                    final String key = PROPS_PREFIX + k;
-                    w.trace("setting sysprop {} = '{}'", key, v);
-                    System.setProperty(key, v.toString());
-                });
+    public static synchronized void addSysProps(final String propsFile) {
+        try (InputStream input = PropsLoader.class.getClassLoader().getResourceAsStream(propsFile)) {
+            var props = new Properties();
+            props.load(input);
+            props.forEach( (k, v) -> {
+                final String key = PROPS_PREFIX + k;
+                w.trace("setting sysprop {} = '{}'", key, v);
+                System.setProperty(key, v.toString());
+            });
 
-            } catch (IOException e) {
-                w.error(e.getMessage());
-            }
+        } catch (IOException e) {
+            w.error(e.getMessage());
         }
-
     }
 
 }
